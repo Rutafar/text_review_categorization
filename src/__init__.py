cfaa4ea8f82c99718728038b7d760843d16b87fc
rutafar <11135577+Rutafar@users.noreply.github.com>
2018-04-12 21:01:34 +0100

    trying pandas

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ import json
 import os.path
 from review.Review import create_review_from_sample
 import datetime
+import pandas as pd
 from features.normalize import remove_stopwords, letters_only, lower_only
 
 
@@ -11,7 +12,11 @@ from features.normalize import remove_stopwords, letters_only, lower_only
 #path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data\\raw\\reviews_Automotive.json")
 reviews_set = set()
 start =datetime.datetime.now()
-with open("C:\\Users\Xavaios\\Documents\\Fac\\2 semestre\\TM\\TA_project%20_1\\data\\raw\\reviews_Automotive.json") as json_file:
+with open("C:\\Users\\Serras\\Documents\\Fac\\2s\\TA\\textanalytics1\\data\\raw\\reviews_Video_Games.json", encoding="utf8") as json_file:
+    dataframe = pd.read_json(json_file, lines=True)
+
+'''
+with open("C:\\Users\Xavaios\\Documents\\Fac\\2 semestre\\TM\\TA_project%20_1\\data\\raw\\reviews_Automotive.json", encoding="utf8") as json_file:
     for line in json_file:
         sample = json.loads(line)
         if "reviewerName" in sample:
@@ -26,3 +31,4 @@ for review in reviews_set:
     text = lower_only(text)
     text  = remove_stopwords(text)
 print(datetime.datetime.now() - start)
+'''

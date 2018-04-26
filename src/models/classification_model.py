44c733d722ba041460399b7ebd5c23d1377bd028
rutafar <11135577+Rutafar@users.noreply.github.com>
2018-04-26 11:03:03 +0100

    metrics

--- src/models/classification_model.py
+++ src/models/classification_model.py
@@ -9,12 +9,7 @@ def lsa(matrix, comppnents):
     ls = TruncatedSVD(n_components=comppnents)
     print('fitting')
     fit = ls.fit_transform(matrix)
-    '''
-    with open(get_file_path('explained_nouns_back.txt'), 'a') as f:
-        f.write(str(comppnents) + '\n')
-        f.write(' '.join(str(i) for i in ls.explained_variance_ratio_) + '\n')
-        f.write(' '.join(str(ls.explained_variance_ratio_.sum())) + '\n')
-    '''
+    print(str(ls.explained_variance_ratio_.sum()))
     return fit
 
 def stand_matrix(data):
--- src/run_exploration.py
+++ src/run_exploration.py
@@ -1,11 +1,12 @@
 from src.data.import_dataset import import_cleaned_training_set, import_cleaned_testing_set
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix, f1_score
 from data.export_dataset import export_comments
 from models.classification_model import lsa
 from features.explore import bag_of_words, only_nouns, tf_idf
 import matplotlib.pyplot as plt
 from sklearn import svm
 from datetime import datetime
+import numpy as np
 
 def main():
     start = datetime.now()
@@ -20,12 +21,13 @@ def main():
     categories_training = extract_categories_from_reviews(training)
     categories_testing = extract_categories_from_reviews(testing)
     print('Generating bag of words')
-
+    categories_training = np.asarray(categories_training)
+    categories_testing = np.asarray(categories_testing)
     bow_vectorizer_training, bow_features_training = bag_of_words(comments_training)
     bow_vectorizer_testing, bow_features_testing = bag_of_words(comments_testing)
     print(datetime.now() - start)
 
-    print(bow_features_testing.shape)
+    #print(bow_features_testing.shape)
     '''
     nouns = only_nouns(comments)
     print(datetime.now() - start)
@@ -33,11 +35,12 @@ def main():
     '''
 
     print('Lsa 500 - training')
-    reduced_training = lsa(bow_features_training, 500)
+    reduced_training = lsa(bow_features_training, 150)
     print(datetime.now() - start)
     print('Lsa 500 - testing')
-    reduced_testing = lsa(bow_features_testing, 500)
+    reduced_testing = lsa(bow_features_testing, 150)
     print(datetime.now() - start)
+    #print(reduced_training, categories_training)
     print('Model part')
     train_model(reduced_training, categories_training, reduced_testing, categories_testing)
     print(datetime.now() - start)
@@ -56,18 +59,27 @@ def extract_comments_from_reviews(dataset):
     return comments_only
 
 def extract_categories_from_reviews(dataset):
+
     categories_only = [review.category for review in dataset]
     return categories_only
 
 
 def train_model(training, training_categories, test, test_categories):
+    start = datetime.now()
     clf = svm.SVC(kernel='linear', C=1.0)
     print('Fitting')
+
     clf.fit(training, training_categories)
+    print(datetime.now()-start)
     print('Predicting')
     predicted = clf.predict(test)
-    print("Predicted: " + predicted)
+    print(datetime.now()-start)
+    #print("Predicted: " + predicted)
     print("Accuracy: " + str(accuracy_score(test_categories, predicted)))
+    print("Used Confuse, it was very effective" + confusion_matrix(test_categories, predicted))
+    print("F1 Score - Vettel wins" + f1_score(test_categories, predicted))
+
+
 
 if __name__ == '__main__':
     main()
